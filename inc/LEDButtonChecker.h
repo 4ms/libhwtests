de6937c798a1ac384c629ad831ad454a5e014ec0
Dan Green <danngreen1@gmail.com>
2020-05-22 18:08:57 -0700

    LEDButtonChecker virtual class (WIP)

--- inc/LEDButtonChecker.h
+++ inc/LEDButtonChecker.h
@@ -1,20 +1,34 @@
 #pragma once
 #include "libhwtests_common.h"
 
-class LEDButtonChecker {
+class ILEDButtonChecker {
 public:
-	LEDButtonChecker(unsigned num_buttons);
-	void assign_button_read_func(ButtonReadFuncType func);
-	void assign_button_led_func(ButtonLEDSetFuncType func);
-
+	ILEDButtonChecker(uint8_t num_channels);
+	bool check();
 	void reset();
-	void run_check();
-	bool check_done();
+	void set_min_steady_state_time(uint32_t min_hold_time);
+
+	enum class ErrorType {
+		None,
+		StuckDown,
+		NoisyPress,
+		NoisyRelease,
+		MultipleHighs
+	};
+	ErrorType get_error();
+
+protected:
+	virtual bool _read_button(uint8_t gate_num) = 0;
+	virtual void _set_error_indicator(uint8_t channel, ErrorType err) = 0;
+	virtual void _set_indicator(uint8_t indicate_num, bool newstate) = 0;
+	virtual void _check_max_one_pin_changed() = 0;
 
 private:
+	enum ErrorType _error;
 	uint8_t _num_buttons;
-	uint32_t _buttons_state;
-	ButtonReadFuncType _button_read;
-	ButtonLEDSetFuncType _set_led;
+	uint8_t _cur_test_chan;
+	unsigned _test_state;
+	unsigned _min_hold_time;
+	void _check_current_button();
 };
 
--- src/LEDButtonChecker.cc
+++ src/LEDButtonChecker.cc
@@ -1,45 +1,63 @@
 #include "LEDButtonChecker.h"
 
-LEDButtonChecker::LEDButtonChecker(unsigned num_buttons) 
-	: _num_buttons(num_buttons) {
-		//static_assert(num_buttons<31,"Max 31 buttons");		
-		if (num_buttons>31)
-			num_buttons=31;
-		_buttons_state = 0;
-	}
+ILEDButtonChecker::ILEDButtonChecker(unsigned num_buttons)
+	: _num_buttons(num_buttons)
+	, _min_hold_time(100)
+	{ //assert(num_buttons<=32) }
 
-void LEDButtonChecker::assign_button_read_func(ButtonReadFuncType func) {
-	_button_read = func;
+void ILEDButtonChecker::reset() {
+	uint32_t coverage;
+	if (_num_channels==32)
+		coverage=0xFFFFFFFF;
+	else
+		coverage = (1<<_num_buttons)-1;
+
+	_cur_test_chan = 0;
+	_test_state = 0;
+	_error = ErrorType::None;
+
+	for (unsigned i=0; i<_num_buttons; i++) {
+		_set_indicator(i, true);
+		_set_error_indicator(i, ErrorType::None);
+	}
 }
 
-void LEDButtonChecker::assign_button_led_func(ButtonLEDSetFuncType func) {
-	_set_led = func;
+bool ILEDButtonChecker::check() {
+	_check_current_button();
+	_check_max_one_pin_changed();
+	return (_cur_test_chan >= _num_buttons);
 }
 
-void LEDButtonChecker::reset() {
-	for (unsigned button_num=0; button_num<_num_buttons; button_num++) {
-		_set_led(button_num, true);
-		_buttons_state |= (1<<button_num);
+void ILEDButtonChecker::_check_current_button() {
+	bool but_pressed = _read_button(_cur_test_chan);
+	_set_indicator(_cur_test_chan, but_pressed);
+	if (_test_state == 0) {
+		if (but_pressed) {
+			_test_state = 1;
+			_debounce_timer = 0;
+		}
 	}
-}
-
-void LEDButtonChecker::run_check() {
-	uint8_t buttons_pressed = 0;
-	for (uint8_t button_num=0; button_num<_num_buttons; button_num++) {
-		if (_button_read(button_num))
-			buttons_pressed++;
-	}
-	if (buttons_pressed == 1) {
-		for (uint8_t button_num=0; button_num<_num_buttons; button_num++) {
-			if (_button_read(button_num)) {
-				_set_led(button_num, false);
-				_buttons_state &= ~(1<<button_num);
+	else if (_test_state == 1) {
+		_debounce_timer++;
+		if (!but_pressed) {
+			if (_debounce_timer < _min_hold_time)
+				_error = ErrorType::NoisyPress;
+			else {
+				_test_state = 2;
+				_debounce_timer = 0;
 			}
 		}
 	}
+	else if (_test_state == 2) {
+		if (!but_pressed)
+			_not_pressed_timer
+	}
 }
 
-bool LEDButtonChecker::check_done() {
-	return (_buttons_state == 0);
+void ILEDButtonChecker::set_min_steady_state_time(uint32_t min_hold_time) {
+	_min_hold_time = min_hold_time;
 }
 
+ILEDButtonChecker::ErrorType ILEDButtonChecker::get_error() {
+	return _error;
+}
