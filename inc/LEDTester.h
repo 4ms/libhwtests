763ba962a1262a383c24bda56a4aade3c81bfab0
Dan Green <danngreen1@gmail.com>
2020-10-15 15:03:45 -0700

    Created ILEDTester base class

--- inc/LEDTester.h
+++ inc/LEDTester.h
@@ -1,9 +1,32 @@
 #pragma once
 #include "libhwtests_common.h"
 
+// LED Tester base class
+// Create a derived class that implements set_led(int, bool)
+// and pause_between_steps()
+class ILEDTester {
+public:
+	ILEDTester(uint8_t num_leds)
+	: _num_leds(num_leds)
+	{ }
+
+	virtual void set_led(int led_id, bool turn_on) = 0;
+	virtual void pause_between_steps() = 0;
+	void reset();
+	bool is_done();
+	void next_led();
+	void run_test();
+
+private:
+	uint8_t _num_leds;
+	uint8_t _next_led;
+	uint8_t _cur_led;
+};
+
+//Deprecated (non-virtual class using function pointer)
 class LEDTester {
 public:
-	LEDTester(uint8_t num_leds) 
+	LEDTester(uint8_t num_leds)
 	: _num_leds(num_leds)
 	{}
 
--- src/LEDTester.cc
+++ src/LEDTester.cc
@@ -1,5 +1,37 @@
 #include "LEDTester.h"
 
+void ILEDTester::reset() {
+	_next_led = 0;
+	_cur_led = 0;
+	for (uint8_t i=0; i<_num_leds; i++) {
+		set_led(i, false);
+	}
+}
+
+bool ILEDTester::is_done() {
+	if (_next_led>=_num_leds)
+		return true;
+	else
+		return false;
+}
+
+void ILEDTester::next_led() {
+	if (is_done())
+		return;
+	set_led(_cur_led, false);
+	set_led(_next_led, true);
+	_cur_led = _next_led;
+	_next_led++;
+}
+
+void ILEDTester::run_test() {
+	reset();
+	while (!is_done()) {
+		pause_between_steps();
+		next_led();
+	}
+}
+
 void LEDTester::assign_led_onoff_func(IndicatorOnOffFuncType func) {
 	_set_led = func;
 }
@@ -15,7 +47,7 @@ void LEDTester::reset() {
 bool LEDTester::is_done() {
 	if (_next_led>=_num_leds)
 		return true;
-	else 
+	else
 		return false;
 }
 
