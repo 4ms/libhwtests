b87faf34677ad7a8949a21e42536d62039f34595
Dan Green <danngreen1@gmail.com>
2022-09-26 17:26:05 -0700

    Formatted CodecCallbacks, and fixed DLD TwoCodec to work with 16 bit samples

--- src/CodecCallbacks.cc
+++ src/CodecCallbacks.cc
@@ -3,15 +3,15 @@
 
 TestRampUpOscillator::TestRampUpOscillator(float freqHz, float max, float min, float initial_phase, float sample_rate)
 	: OutputStream(sample_rate)
-	, _inc(freqHz/sample_rate)
+	, _inc(freqHz / sample_rate)
 	, _max(max)
 	, _min(min)
-	, _cur_phase(initial_phase)
-{}
+	, _cur_phase(initial_phase) {
+}
 
 void TestRampUpOscillator::init(float freqHz, float max, float min, float initial_phase, float sample_rate) {
 	_sample_rate = sample_rate;
-	_inc = freqHz/sample_rate;
+	_inc = freqHz / sample_rate;
 	_max = max;
 	_min = min;
 	_cur_phase = initial_phase;
@@ -22,25 +22,26 @@ float TestRampUpOscillator::update() {
 	while (_cur_phase > 1.f)
 		_cur_phase -= 1.f;
 
-	return ((_cur_phase * (_max-_min)) + _min);
+	return ((_cur_phase * (_max - _min)) + _min);
 }
 
 TestGateOscillator::TestGateOscillator(float sample_rate)
-	: OutputStream(sample_rate)
-{}
+	: OutputStream(sample_rate) {
+}
 
-TestGateOscillator::TestGateOscillator(float freqHz, float pw, float max, float min, float initial_phase, float sample_rate)
+TestGateOscillator::TestGateOscillator(
+	float freqHz, float pw, float max, float min, float initial_phase, float sample_rate)
 	: OutputStream(sample_rate)
-	, _inc(freqHz/sample_rate)
+	, _inc(freqHz / sample_rate)
 	, _pw(pw)
 	, _max(max)
 	, _min(min)
-	, _cur_phase(initial_phase)
-{}
+	, _cur_phase(initial_phase) {
+}
 
 void TestGateOscillator::init(float freqHz, float pw, float max, float min, float initial_phase, float sample_rate) {
 	_sample_rate = sample_rate;
-	_inc = freqHz/sample_rate;
+	_inc = freqHz / sample_rate;
 	_pw = pw;
 	_max = max;
 	_min = min;
@@ -56,24 +57,23 @@ float TestGateOscillator::update() {
 }
 
 SkewedTriOsc::SkewedTriOsc(float sample_rate)
-	: OutputStream(sample_rate)
-{}
+	: OutputStream(sample_rate) {
+}
 
 SkewedTriOsc::SkewedTriOsc(float freqHz, float riseRatio, float max, float min, float initial_phase, float sample_rate)
 	: OutputStream(sample_rate)
-	, _rise_inc(freqHz / (sample_rate*riseRatio))
-	, _fall_inc(freqHz / (sample_rate*(1.0f-riseRatio)))
+	, _rise_inc(freqHz / (sample_rate * riseRatio))
+	, _fall_inc(freqHz / (sample_rate * (1.0f - riseRatio)))
 	, _max(max)
 	, _min(min)
-	, _cur_phase(initial_phase)
-{
+	, _cur_phase(initial_phase) {
 	_is_rising = (_cur_phase < riseRatio);
 }
 
 void SkewedTriOsc::init(float freqHz, float riseRatio, float max, float min, float initial_phase, float sample_rate) {
 	_sample_rate = sample_rate;
-	_rise_inc = freqHz / (_sample_rate*riseRatio);
-	_fall_inc = freqHz / (_sample_rate*(1.0f-riseRatio));
+	_rise_inc = freqHz / (_sample_rate * riseRatio);
+	_fall_inc = freqHz / (_sample_rate * (1.0f - riseRatio));
 	_max = max;
 	_min = min;
 	_cur_phase = initial_phase;
@@ -94,30 +94,32 @@ float SkewedTriOsc::update() {
 		_is_rising = true;
 		_cur_phase = 0.0f;
 	}
-	return ((_cur_phase * (_max-_min)) + _min);
+	return ((_cur_phase * (_max - _min)) + _min);
 }
 
 CenterFlatRamp::CenterFlatRamp(float sample_rate)
-	: OutputStream(sample_rate)
-{}
+	: OutputStream(sample_rate) {
+}
 
-CenterFlatRamp::CenterFlatRamp(float freqHz, float flat_width, float max, float min, float initial_phase, float sample_rate)
+CenterFlatRamp::CenterFlatRamp(
+	float freqHz, float flat_width, float max, float min, float initial_phase, float sample_rate)
 	: OutputStream(sample_rate)
 	, _inc(freqHz / sample_rate)
-	, _flat_start(0.5f - flat_width*0.5f)
-	, _flat_end(0.5f + flat_width*0.5f)
-	, _nonflat_slope(0.5f/_flat_start)
+	, _flat_start(0.5f - flat_width * 0.5f)
+	, _flat_end(0.5f + flat_width * 0.5f)
+	, _nonflat_slope(0.5f / _flat_start)
 	, _max(max)
 	, _min(min)
-	, _cur_phase(initial_phase)
-{}
+	, _cur_phase(initial_phase) {
+}
 
-void CenterFlatRamp::init(float freqHz, float flat_width, float max, float min, float initial_phase, float sample_rate) {
+void CenterFlatRamp::init(
+	float freqHz, float flat_width, float max, float min, float initial_phase, float sample_rate) {
 	_sample_rate = sample_rate;
 	_inc = freqHz / _sample_rate;
-	_flat_start = 0.5f - flat_width*0.5f;
-	_flat_end = 0.5f + flat_width*0.5f;
-	_nonflat_slope = 0.5f/_flat_start;
+	_flat_start = 0.5f - flat_width * 0.5f;
+	_flat_end = 0.5f + flat_width * 0.5f;
+	_nonflat_slope = 0.5f / _flat_start;
 	_max = max;
 	_min = min;
 	_cur_phase = initial_phase;
@@ -128,7 +130,7 @@ float CenterFlatRamp::update() {
 	while (_cur_phase > 1.0f) {
 		_cur_phase -= 1.0f;
 	}
-	
+
 	float out;
 	if (_cur_phase < _flat_start)
 		out = _nonflat_slope * _cur_phase;
@@ -139,61 +141,56 @@ float CenterFlatRamp::update() {
 	else
 		out = _nonflat_slope * (_cur_phase - _flat_end) + 0.5f;
 
-	return ((out * (_max-_min)) + _min);
+	return ((out * (_max - _min)) + _min);
 }
 
 void CodecCallbacks_TwoCodecs::testWavesOut(SampleT *src, SampleT *dst, uint16_t sz, uint8_t channel) {
 	OutputStream *testWaveL, *testWaveR;
-	if (channel==0) {
+	if (channel == 0) {
 		testWaveL = leftOutCodec1;
 		testWaveR = rightOutCodec1;
-	}
-	else if (channel==1) {
+	} else if (channel == 1) {
 		testWaveL = leftOutCodec2;
 		testWaveR = rightOutCodec2;
-	}
-	else return;
-
-	if (testWaveL==nullptr || testWaveR==nullptr)
+	} else
 		return;
 
-	for (uint16_t i=0; i<sz/2; i++)
-	{
+	if (testWaveL == nullptr || testWaveR == nullptr)
+		return;
+
+	for (uint16_t i = 0; i < sz; i++) {
 		float leftOut = testWaveL->update();
-		*dst++ = (int16_t)leftOut;
-		*dst++ = 0;
+		*dst++ = (SampleT)leftOut;
+		// *dst++ = 0;
 
 		float rightOut = testWaveR->update();
 		*dst++ = (int16_t)rightOut;
-		*dst++ = 0;
+		// *dst++ = 0;
 	}
-	(void)(*src);//unused
+	(void)(*src); //unused
 }
 
 void CodecCallbacks_TwoCodecs::passthruPlusTestWave(SampleT *src, SampleT *dst, uint16_t sz, uint8_t channel) {
 	OutputStream *testWaveL, *testWaveR;
-	if (channel==0) {
+	if (channel == 0) {
 		testWaveL = leftOutCodec1;
 		testWaveR = rightOutCodec1;
-	}
-	else if (channel==1) {
+	} else if (channel == 1) {
 		testWaveL = leftOutCodec2;
 		testWaveR = rightOutCodec2;
-	}
-	else return;
-
-	if (testWaveL==nullptr || testWaveR==nullptr)
+	} else
 		return;
 
-	for (uint16_t i=0; i<sz/2; i++)
-	{
+	if (testWaveL == nullptr || testWaveR == nullptr)
+		return;
+
+	for (uint16_t i = 0; i < sz; i++) {
 		float leftOut = testWaveL->update();
-		*dst++ = ((int16_t)leftOut)/2 + (*src++);
-		*dst++ = *src++;
+		*dst++ = ((SampleT)leftOut) / 2 + (*src++);
+		// *dst++ = *src++;
 
 		float rightOut = testWaveR->update();
-		*dst++ = ((int16_t)rightOut)/2 + (*src++);
-		*dst++ = *src++;
+		*dst++ = ((SampleT)rightOut) / 2 + (*src++);
+		// *dst++ = *src++;
 	}
 }
-
