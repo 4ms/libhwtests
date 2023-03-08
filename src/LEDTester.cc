37215deb7f0311c8f413413bf1b110232825fcaa
Dan Green <danngreen1@gmail.com>
2023-03-08 12:33:47 -0800

    Fix ILEDTester, and simplify (was skipping last LED)

--- src/LEDTester.cc
+++ src/LEDTester.cc
@@ -1,43 +1,39 @@
 #include "libhwtests/LEDTester.hh"
 
 void ILEDTester::reset() {
-	_next_led = 0;
 	_cur_led = 0;
-	for (uint8_t i = 0; i < _num_leds; i++) {
+	set_led(0, true);
+	for (uint8_t i = 1; i < _num_leds; i++) {
 		set_led(i, false);
 	}
 }
 
 bool ILEDTester::is_done() {
-	if (_next_led >= _num_leds)
+	if (_cur_led >= _num_leds)
 		return true;
 	else
 		return false;
 }
 
 void ILEDTester::next_led() {
+	set_led(_cur_led, false);
+	_cur_led++;
 	if (is_done())
 		return;
-	set_led(_cur_led, false);
-	set_led(_next_led, true);
-	_cur_led = _next_led;
-	_next_led++;
+	set_led(_cur_led, true);
 }
 
 void ILEDTester::run_test() {
 	reset();
-	next_led();
 	while (!is_done()) {
 		pause_between_steps();
 		next_led();
 	}
 }
 
-void LEDTester::assign_led_onoff_func(IndicatorOnOffFuncType func) {
-	_set_led = func;
-}
+[[deprecated]] void LEDTester::assign_led_onoff_func(IndicatorOnOffFuncType func) { _set_led = func; }
 
-void LEDTester::reset() {
+[[deprecated]] void LEDTester::reset() {
 	_next_led = 0;
 	_cur_led = 0;
 	for (uint8_t i = 0; i < _num_leds; i++) {
@@ -45,14 +41,14 @@ void LEDTester::reset() {
 	}
 }
 
-bool LEDTester::is_done() {
+[[deprecated]] bool LEDTester::is_done() {
 	if (_next_led >= _num_leds)
 		return true;
 	else
 		return false;
 }
 
-void LEDTester::next_led() {
+[[deprecated]] void LEDTester::next_led() {
 	if (is_done())
 		return;
 	_set_led(_cur_led, false);
