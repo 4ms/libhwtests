62613353edb874e33c7ea84a5d9c713f17e9ccf9
Dan Green <danngreen1@gmail.com>
2023-05-02 21:56:16 -0700

    formatting

--- class_idea.hh
+++ class_idea.hh
@@ -1,44 +1,47 @@
 #include <concepts>
 
 enum class ButtonError {
-  None,
-  StuckDown,
-  NoisyPress,
-  NoisyRelease,
-  MultipleHighs,
-  Unknown,
+	None,
+	StuckDown,
+	NoisyPress,
+	NoisyRelease,
+	MultipleHighs,
+	Unknown,
 };
 
-template <typename T>
+template<typename T>
 concept BoardC = requires(T t, unsigned chan) {
-                   { t.read_button(chan) } -> std::same_as<bool>;
-                   t.set_indicator(chan, bool{});
-                   t.set_error_indicator(chan, ButtonError{});
-                 };
+	{
+		t.read_button(chan)
+	} -> std::same_as<bool>;
+	t.set_indicator(chan, bool{});
+	t.set_error_indicator(chan, ButtonError{});
+};
 
-template <BoardC Board> class ButtonTester {
-  Board board;
-  unsigned cur_chan = 0;
+template<BoardC Board>
+class ButtonTester {
+	Board board;
+	unsigned cur_chan = 0;
 
 public:
-  bool check() {
-    board.read_button(cur_chan);
-    cur_chan++;
-    return true;
-  }
+	bool check() {
+		board.read_button(cur_chan);
+		cur_chan++;
+		return true;
+	}
 };
 
 ////// in project:
 
 struct SWNButtonIO {
-  bool read_button(unsigned num) { return false; }
-  void set_indicator(unsigned num, bool high) {}
-  void set_error_indicator(unsigned num, ButtonError err) {}
+	bool read_button(unsigned num) { return false; }
+	void set_indicator(unsigned num, bool high) {}
+	void set_error_indicator(unsigned num, ButtonError err) {}
 };
 
 void test_buttons() {
-  ButtonTester<SWNButtonIO> checker;
+	ButtonTester<SWNButtonIO> checker;
 
-  while (!checker.check()) {
-  }
+	while (!checker.check()) {
+	}
 }
--- inc/libhwtests/HardwareTestUtil.hh
+++ inc/libhwtests/HardwareTestUtil.hh
@@ -4,23 +4,21 @@
 
 template<typename T>
 concept HWBaseC = requires(T t) {
-					  { T::main_button_pressed() } -> std::same_as<bool>;
-					  T::delay_ms(uint32_t{});
-					  T::set_main_button_led(bool{});
-				  };
+	{
+		T::main_button_pressed()
+	} -> std::same_as<bool>;
+	T::delay_ms(uint32_t{});
+	T::set_main_button_led(bool{});
+};
 
 // Establishes the minimum needed to perform a hardware test: one button and one
 // LED, and a way to delay
 template<HWBaseC Base>
 struct HardwareTestUtil {
 
-	static bool main_button_pressed() {
-		return Base::main_button_pressed();
-	}
+	static bool main_button_pressed() { return Base::main_button_pressed(); }
 
-	static void delay_ms(uint32_t ms) {
-		Base::delay_ms(ms);
-	}
+	static void delay_ms(uint32_t ms) { Base::delay_ms(ms); }
 
 	static void pause_until_button_pressed() {
 		Base::delay_ms(10);
